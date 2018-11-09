002c4667241a6016a47d18365b08d7aa57f32228
Jesse Sheehan <jesse@sheehan.nz>
2018-11-10 12:17:15 +1300

    First commit.

---
+++ src/components/UserList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>User</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserList;
